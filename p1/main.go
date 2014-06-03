6c1bec717a66f5edef7e74ab3d5e80bdad812207
Corey Prak <PrikPrak@users.noreply.github.com>
2014-06-03 16:51:05 -0400

    p1/main.go: Changed comment format.

--- p1/main.go
+++ p1/main.go
@@ -6,7 +6,8 @@ Comments:
 06/02/2014 - Added comments to describe the program. 
 */
 
-/* Every program must include a main package, along with a function named main that does not take any arguments. */
+/* Every program must include a main package, along with a function named main 
+   that does not take any arguments. */
 package main
 
 /* Import the 'fmt' package. All packages must be included with quotes around it */
@@ -14,6 +15,9 @@ import "fmt"
 
 func main() {
 
-  /* We are calling the Println that is apart of the fmt package. As you can see, the name of the package and a period comes before the function. There are more ways to access content of a package, more on that later. Lastly, we provide the string literal "Hello, world!" */
-  fmt.Println("Hello, world!")
+/* We are calling the Println that is apart of the fmt package. As you can see, 
+   the name of the package and a period comes before the function. There are more 
+   ways to access content of a package, more on that later. Lastly, we provide the 
+   string literal "Hello, world!" */
+   fmt.Println("Hello, world!")
 }
