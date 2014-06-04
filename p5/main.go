457ea2c5e64f44e2fe48bbc154937d431c4042cc
PrikPrak <PrikPrak@users.noreply.github.com>
2014-06-03 21:56:04 -0400

    p5/main.go: Added comments and changed code to function as intended.

--- p5/main.go
+++ p5/main.go
@@ -2,7 +2,14 @@
 Problem #5: The Sum of Two Values
 Author: Corey Prak
 Date Created: 04/03/2014
+
+Description: Use Scanf to read in two integers, store the sum in a 
+variable of the same word, print the variable to stdout. 
+
 Comments:
+06/03/2014
+
+Added comments, changed code drastically. 
 */
 
 package main
@@ -10,13 +17,23 @@ package main
 import "fmt"
 
 func main(){
-  var n1 int
-  n2 := 0
+ 
+//  Declare two variables which are defaulted to a value of 0.  
+  var num1, num2, sum int
 
-  n1 = 0
-
+/*  Request for the user to enter two integer values. As needed by 
+    the Scanf function, the next two arguments must be an address to 
+    a particular location in memory that is of a matching type. %d
+    requires an integer value, and that is what was provided. Also,
+    note how Scanf is able to read in more than one integer. The format
+    specifiers must be separated by a space when doing this.  */
   fmt.Println("Please enter two integer values:")
-  fmt.Scanf("%d%d", &n1, &n2)
+  fmt.Scanf("%d %d", &num1, &num2)
 
-  fmt.Printf("The sum of the two integers is %d.\n", (n1 + n2))
+  sum = num1 + num2
+
+/*  Use the %d format specifier to ensure that the next argument is 
+    an expression that reports out a value with the particular type,
+    in this case, an integer.  */
+  fmt.Printf("The sum of the two integers is %d.\n", sum)
 }
