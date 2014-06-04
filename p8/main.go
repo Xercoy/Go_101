f6a43f47c9f16a9029452ab4081c0a7018938346
PrikPrak <PrikPrak@users.noreply.github.com>
2014-06-04 02:28:57 -0400

    p8/main.go: Added comments and refactored.

--- p8/main.go
+++ p8/main.go
@@ -1,22 +1,28 @@
-/*
+/*******************************************************************************
 Problem #8: One Horizontal Line of Asterisks
 Author: Corey Prak
 Date Created: 04/03/2014
 
-Description
-Using the emacs editor, create a file called testdata8 that has a single integer value stored into it. The integer will be a number between 1 and 30 inclusive. Create a C program that uses the fscanf function to read in this integer value and then prints a horizontal line containing that many asterisks’ (*). Use a for-loop in your program. 
+Description:
+Create a file called testdata8 that has a single integer value stored into it. 
+The integer will be a number between 1 and 30 inclusive. Create a C program 
+that uses the fscanf function to read in this integer value and then prints a 
+horizontal line containing that many asterisks’ (*). Use a for-loop in your 
+program. 
 
 Comments:
 
-Attempted to cast string from file read to integer. didn't work out, was 
-indicated by printf %T format specifier that it was of type []uint8, however
-error message upon cast attempt claimed it was of type []byte. I can't 
-explain this. 
+06/04/2014
 
-Decided to dig deeper and found the good ol' Atoi function. 
+- Added comments.
 
-Dug a little deeper and found that FSCANF EXISTS. WHUUUT
-*/
+- Made HUGE changes to code. 
+
+- Decided not to remove short declarations to show how elegant the code could 
+  be. 
+
+- Must REMEMBER TO CLOSE THE FILE. I can't seem to get that into my head. 
+*******************************************************************************/
 
 package main
 
@@ -26,26 +32,30 @@ import(
 )
 
 func main(){
-  fileNum := 0
-  // Iterator for the for loop.
-  i := 0;
+
+  /* First variable should be self explanatory, second is the interator for i. */
+  var numberFromFile, i int
 
   // Open the file, return address to filePtr, fErr if possible.
 	filePtr, fErr := os.Open("testdata8")
-  
   if fErr != nil {
 		fmt.Println(fErr)
+    return
   }
 
-  _, scanErr := fmt.Fscanf(filePtr, "%d", &fileNum)
+  /* We're not worried sbout the first return value of the Fscanf function 
+     just yet, so omit it with an underscore. */
+  _, scanErr := fmt.Fscanf(filePtr, "%d", &numberFromFile)
   if scanErr != nil {
     fmt.Println(scanErr)
+    return
   }
-  
 
-  for i = 0; i < fileNum  ; i = i+1 {
+  for i = 0; i < numberFromFile; i++ {
 	  fmt.Printf("*")
   }
   
-		fmt.Printf("\n")
+	fmt.Printf("\n")
+
+  filePtr.Close()
 }
